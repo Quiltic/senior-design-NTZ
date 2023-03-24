7b7897f47a04f2b2f816aef83a17c728ee116859
Justin Nordeman <nordemja@mail.uc.edu>
2023-03-24 14:57:50 -0400

    UI components in place

--- src/ui.py
+++ src/ui.py
@@ -44,11 +44,32 @@ btn = Button(master, text = "Browse Files", command = browse_files)
 btn.place(x=750, y=100)
 
 
+ 
+# Create the text widget for two lines of text
+text_widget = Text(master, height=25, width=50)
+text_widget_2 = Text(master, height=25, width=50)
+ 
+# Pack it into our tkinter application
+text_widget.place(x=25, y=200)
+text_widget_2.place(x=450, y=200)
+ 
+# Insert text into the text widget
+# tk.END specifies insertion after the last character in our buffer
+text_widget.insert(END, "First Line - Hello from AskPython\nSecond Line - Hi")
+text_widget.configure(state=DISABLED)
 
+text_widget_2.insert(END, "First Line - Hello from AskPython\nSecond Line - Hi")
+text_widget_2.configure(state=DISABLED)
 
 # Create a File Explorer label
 label_file_explorer = Label(master, text = None)
 label_file_explorer.pack(pady = 10)
+
+transcription_label = Label(master, text = "TRANSCRIBED FILE")
+transcription_label.place(x=150, y = 170)
+
+notes_label = Label(master, text = "NOTES")
+notes_label.place(x=600, y = 170)
   
       
 
